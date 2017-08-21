a454386174ff2af5ca864639486df77c7c9ad67d
Jacob Penny <jacob.penny@gmail.com>
2017-08-21 11:38:52 -0400

    Initial implementation of repos endpoint

--- api/src/routes/repos.js
+++ api/src/routes/repos.js
@@ -1,14 +1,24 @@
 const express = require('express');
+const rp = require('request-promise');
+const localRepos = require('../../../data/repos.json');
 
+const REPOS_ENDPOINT = 'https://api.github.com/users/silverorange/repos';
 const router = express.Router(); // eslint-disable-line new-cap
 
-router.get(
-  '/',
-  (req, res) => {
+router.get('/', (req, res, next) => {
+  rp({
+    uri: REPOS_ENDPOINT,
+    headers: { 'User-Agent': 'Request-Promise' },
+    json: true,
+  }).then((apiRepos) => {
+    const reposWithoutForked = localRepos.concat(apiRepos).filter(repo => repo.fork === false);
+
     res.header('Content-Type', 'application/json');
     res.status(200);
-    res.send('[]');
-  }
-);
+    res.send(reposWithoutForked);
+  }).catch((err) => {
+    next(err);
+  });
+});
 
 module.exports = router;
